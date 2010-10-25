4a217848d1b80b36581afe840943ddcbad6c81b8
Masahiro Namba <qt.noc06140728@gmail.com>
2010-10-25 19:01:03 +0900

    レポートの歩数をカンマ表示に変更
    DJango のカスタムフィルタを定義し、
    テンプレート側でカンマ編集ができるようにした。

--- src/customfilters_m.py
+++ src/customfilters_m.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import datetime
+import re
 from google.appengine.ext import webapp
 
 register = webapp.template.create_template_register()
@@ -8,5 +9,10 @@ register = webapp.template.create_template_register()
 def sjis(body):
 	return body.encode('Shift_JIS')
 
+def commafmt(value):
+    return re.compile(r'(\d)(?=(?:\d{3})+$)').sub(r'\1,', str(value))
+
 register.filter(sjis)
+register.filter(commafmt)
+
 
