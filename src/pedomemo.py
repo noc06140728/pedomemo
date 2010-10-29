126caefbc91ab9bf9ec4b64b81ae7bf55131f4ff
Masahiro Namba <qt.noc06140728@gmail.com>
2010-10-29 13:42:20 +0900

    履歴表示画面への平均歩数表示の追加

--- src/pedomemo.py
+++ src/pedomemo.py
@@ -224,7 +224,8 @@ class HistoryPage(BaseHandler):
         campaign_steps = user.getStepSummary(campaign_term)
         campaign_rank = Ranking(campaign_term).getRank(user)
         users_count = User.all().count()
-        self.write_response_template({'user': user, 'records': records, 'monthly_steps': monthly_steps, 'monthly_rank': monthly_rank, 'campaign_steps': campaign_steps, 'campaign_rank': campaign_rank, 'users_count': users_count})
+        step_count = user.getStepRecords(campaign_term).count()
+        self.write_response_template({'user': user, 'records': records, 'monthly_steps': monthly_steps, 'monthly_rank': monthly_rank, 'campaign_steps': campaign_steps, 'campaign_rank': campaign_rank, 'users_count': users_count, 'average': campaign_steps/step_count})
 
 class RankingPage(BaseHandler):
     def get(self):
