8543b96be8f74acb1337ef461cbd22b678840b8b
Masahiro Namba <qt.noc06140728@gmail.com>
2010-11-07 08:35:00 +0900

    レポート画面の表示を携帯とPCと分離
    UserAgent の内容に応じて、PC と携帯を分けて表示するようにした。
    携帯ページからは、メニューURL をメール転送できるようにした。

---
+++ src/useragent_m.py
@@ -0,0 +1,37 @@
+import re
+
+class UserAgent:
+
+    user_agent = ""
+    carrier = None
+
+    def setUserAgent(self, agent):
+        self.user_agent = agent
+
+        if(re.match("^DoCoMo", agent)):
+            self.carrier = "DOCOMO"
+        elif(re.match("^J-PHONE|^Vodafone|^SoftBank", agent)):
+            self.carrier = "SOFTBANK"
+        elif(re.match("^UP.Browser|^KDDI", agent)):
+            self.carrier = "KDDI"
+        else:
+            self.carrier = None
+
+    def getUserAgent(self):
+        return self.user_agent
+
+    def getCarrier(self):
+        return self.carrier
+
+    def isDocomo(self):
+        return self.carrier == "DOCOMO"
+
+    def isSoftBank(self):
+        return self.carrier == "SOFTBANK"
+
+    def isKDDI(self):
+        return self.carrier == "KDDI"
+
+    def isMobile(self):
+        return (self.isDocomo() or self.isSoftBank() or self.isKDDI())
+
