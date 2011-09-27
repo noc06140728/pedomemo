2c79f0c594a734412a0bd37a367a6c10c5696dbe
noc <noc@noc-CF-R3FW1AXR.(none)>
2011-09-28 03:32:55 +0900

    タスク用のハンドラのレスポンス除去
    タスク用のハンドラは、レスポンス文字列を返さず、
    ログに出力するように修正した。

--- src/admin.py
+++ src/admin.py
@@ -6,23 +6,26 @@ import wsgiref.handlers
 from google.appengine.ext import webapp
 from google.appengine.api import taskqueue
 
+import logging
 from pedomemo import *
 
 class CountStepsWorker(webapp.RequestHandler):
     def get(self):
+        logging.info('CountStepsWorker started.')
         start_date = parseDate(self.request.get('start'))
         end_date = parseDate(self.request.get('end'))
         StepSummary.countStepRecords(Term(start_date, end_date))
-        self.response.out.write('Calculate steps complate.')
+        logging.info('Calculate steps complate.')
 
 class CountTaskWorker(webapp.RequestHandler):
     def get(self):
+        logging.info('CountTaskWorker started.')
         for task in CountTask.all():
             taskqueue.add(url='/admin/count', method='GET',
                           params={'start': task.start_date.strftime('%Y%m%d'),
                                   'end': task.end_date.strftime('%Y%m%d')})
             task.delete()
-        self.response.out.write('Task added.')
+        logging.info('Task added.')
 
 application = webapp.WSGIApplication([
   ('/admin/count', CountStepsWorker),
